0057245a635021c6d36d6e5573fbd8fde2b90992
Heedong Lee <arkinylee@gmail.com>
2020-03-08 14:37:40 +0900

    Let's Cmake wit Linux

--- [Udemy]AdvancedCPP/Exceptions/ExceptionMain.cpp
+++ [Udemy]AdvancedCPP/Exceptions/ExceptionMain.cpp
@@ -1,18 +1,29 @@
+#include "BasicException.h"
 #include <iostream>
-using namespace std; // for convinience
+#include <string>
 
-void DefinitlyException()
-{
-    auto error = true;
-
-    if (error)
-    {
-        throw 8;
-    }
-}
+using namespace std;
 
 int main()
 {
-    DefinitlyException();
+    try
+    {
+        NestedFunctionAlsoCanCatchException();
+    }
+    catch (int e)
+    {
+        cout << "Error code: " << e << endl;
+    }
+    catch (char const* e)
+    {
+        cout << "Error String : " << e << endl;
+    }
+    catch (string& e)
+    {
+        cout << "Erorr String : " << e << endl;
+    }
+
+    cout << "Still running" << endl;
+
     return 0;
 }
